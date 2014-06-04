9db3bb029b0a5e102a78a403a9cf0d9d460a0ef3
Gerald Loeffler <gerald.loeffler@mulesoft.com>
2014-06-04 15:52:01 +0100

    making Scala class definitions more idiomatic

--- comparisons/classes/subclass/scala.scala
+++ comparisons/classes/subclass/scala.scala
@@ -1,25 +1,17 @@
 class NamedShape(var name: String) {
     var numberOfSides: Int = 0
 
-    def simpleDescription(): String = {
-        return s"A shape with $numberOfSides sides."
-    }
+    def simpleDescription() = s"A shape with $numberOfSides sides."
 }
 
 class Square(var sideLength: Double, name: String)
     extends NamedShape(name) {
-    def area(): Double = {
-        return sideLength * sideLength
-    }
+    
+    numberOfSides = 4
+    
+    def area() = sideLength * sideLength
 
-    def Square(sideLength: Double, name: String) = {
-        this.sideLength = sideLength
-        numberOfSides = 4
-    }
-
-    override def simpleDescription(): String = {
-        return s"A square with sides of length $sideLength."
-    }
+    override def simpleDescription() = s"A square with sides of length $sideLength."
 }
 
 val test = new Square(5.2, "my test square")
